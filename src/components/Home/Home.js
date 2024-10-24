1fe47bdea5dd96fbe879c72efc16925f85effcc3
bhushan-suprgya <bhushansuprgya@gmail.com>
2024-10-25 03:54:10 +0530

    4th commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> BHUSH SUPRGYA</strong>
+                <strong className="main-name"> SUPRGYA BHUSHAN</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
